c810bda0628651fc6cb278fcb4b907daf9be45ee
Karen Zakharyan <Zakharyan.kar@gmail.com>
2021-06-25 18:24:01 +0400

    fix(project): fix responsive bugs

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const burgerMenu = document.querySelector(".burger-menu-btn");
 const navbarMenu = document.querySelector(".navbar-menu");
-
+const container = document.querySelector(".container");
 let open = false;
 
 burgerMenu.onclick = () => {
   if (!open) {
     open = !open;
     navbarMenu.style.display = "block";
+    container.style.height = "80%";
   } else {
     open = !open;
+    navbarMenu.style.display = "none";
+    container.style.height = "84%";
+  }
+};
+
+window.onresize = () => {
+  if (window.innerWidth > 800) {
+    navbarMenu.style.display = "block";
+    container.style.height = "84%";
+  } else {
+    container.style.height = "84%";
     navbarMenu.style.display = "none";
   }
 };
